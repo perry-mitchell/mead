98cde0434178da77ab71dafdb8444754f543bf47
Perry Mitchell <perry@perrymitchell.net>
2017-06-15 23:02:19 +0300

    Add file handling logic

--- source/app/index.js
+++ source/app/index.js
@@ -1,21 +1,121 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, dialog, BrowserWindow, Menu, ipcMain, globalShortcut} = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
+const path = require('path');
+const fs = require('fs');
+const pify = require('pify');
+const debounce = require('debounce');
+const fileUrl = require('file-url');
+const argv = require('minimist')(process.argv.slice(2));
+
+const readFile = pify(fs.readFile);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+// let win
+const windows = {};
 
-function createWindow () {
+ipcMain.on("editorReady", function(event, info) {
+  const { windowID } = info;
+  const { window, filePath } = windows[windowID];
+  if (filePath) {
+    readFile(filePath, "utf8")
+      .then(contents => {
+        window.webContents.send('setContents', {
+          filename: path.basename(filePath),
+          contents
+        });
+      });
+  }
+});
+
+ipcMain.on("save", function(event, info) {
+  const { contents, windowID } = info;
+  const { filePath, window } = windows[windowID];
+  if (filePath) {
+    fs.writeFileSync(filePath, contents, "utf8");
+    windows[windowID].dirty = false;
+    window.webContents.send("setDirty", { dirty: false });
+  } else {
+    dialog.showSaveDialog(window, { title: "Save new markdown file" }, function(filename) {
+      if (filename) {
+        windows[windowID].filePath = filename;
+        fs.writeFileSync(filename, contents, "utf8");
+        windows[windowID].dirty = false;
+        window.webContents.send("setDirty", { dirty: false });
+        window.webContents.send('setFilename', {
+          filename: path.basename(filename)
+        });
+      }
+    });
+  }
+});
+
+ipcMain.on("edit", function(event, info) {
+  const { contents, windowID } = info;
+  // console.log("EDIT", contents, windowID);
+  checkDirty(windowID, contents);
+  // const { dirty, window } = windows[windowID];
+  // if (!dirty) {
+  //   windows[windowID].dirty = true;
+  //   window.webContents.send("setDirty", { dirty: true });
+  // }
+  // let { dirtyCheck } = windows[windowID];
+  // if (filePath && !dirty) {
+  //   // debounce(dirtyCheck, 5000);
+  //   dirtyCheck = dirtyCheck || debounce(() => {
+  //     readFile(filePath).then(readContents => {
+  //       if (contents !== readContents)
+  //     });
+  //   }, 5000);
+  //   dirtyCheck();
+  // }
+});
+
+function checkDirty(windowID, contents) {
+  const { dirty, filePath, window } = windows[windowID];
+  if (filePath) {
+    const dirtyCheck = windows[windowID].dirtyCheck || debounce(() => {
+      readFile(filePath, "utf8").then(readContents => {
+        const isNowDirty = (readContents !== contents);
+        if (isNowDirty !== dirty) {
+          // update
+          windows[windowID].dirty = isNowDirty;
+          window.webContents.send("setDirty", { dirty: isNowDirty });
+        }
+      });
+    }, 1000);
+    dirtyCheck();
+  }
+}
+
+function createWindow(filePath = null) {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  const win = new BrowserWindow({width: 800, height: 600});
+  setMenu();
+  const winID = `win:${Math.random()}`;
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.resolve(__dirname, "../renderer/index.html"),
-    protocol: 'file:',
-    slashes: true
-  }))
+  const url = fileUrl(path.resolve(__dirname, "../renderer/index.html"));
+  win.loadURL(`${url}?id=${winID}`);
+
+  // if (filePath !== null) {
+    // const localPath = path.resolve(process.cwd(), filePath);
+    // win.webContents.on('editorReady', () => {
+    //   console.log("READY");
+    //   readFile(localPath, "utf8")
+    //     .then(contents => {
+    //       console.log("SEND", contents);
+    //       win.webContents.send('setContents', { contents });
+    //     });
+    // });
+  // }
+
+  electronLocalshortcut.register(win, 'CmdOrCtrl+S', () => {
+    saveCurrent();
+  });
+  electronLocalshortcut.register(win, 'CmdOrCtrl+N', () => {
+    createWindow();
+  });
 
   // Open the DevTools.
   win.webContents.openDevTools()
@@ -25,31 +125,72 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    // win = null
+    delete windows[winID];
+  });
+
+  windows[winID] = {
+    window: win,
+    dirty: false,
+    filePath: filePath ? path.resolve(process.cwd(), filePath) : null
+  };
+}
+
+function saveCurrent() {
+  const focusedWindow = BrowserWindow.getFocusedWindow();
+  const windowKey = Object.keys(windows).find(key => windows[key].window === focusedWindow);
+  if (windowKey) {
+    focusedWindow.webContents.send("saveNow");
+  }
+}
+
+function setMenu() {
+  const menuTemplate = [
+    {
+      label: "File",
+      submenu: [
+        { label: "New", click: () => createWindow() },
+        { label: "Save", click: saveCurrent }
+      ]
+    }
+  ];
+  if (process.platform === 'darwin') {
+    menuTemplate.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'},
+        {role: 'quit'}
+      ]
+    })
+  }
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  const filename = argv._[0] || null;
+  createWindow(filename);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  // if (process.platform !== 'darwin') {
     app.quit()
-  }
+  // }
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+// app.on('activate', () => {
+//   // On macOS it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (win === null) {
+//     createWindow()
+//   }
+// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
